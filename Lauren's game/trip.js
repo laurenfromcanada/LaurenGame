f4e999e4dfac990ea1fe4c0f5113e00353d054be
Seth <sethmoffat1@gmail.com>
2022-12-08 19:37:16 -0700

    better

--- Lauren's game/trip.js
+++ Lauren's game/trip.js
@@ -1,2 +1,18 @@
-let salary = 0
-let budget = 0
+let budget = 10000
+
+function budget1(){
+    document.getElementById("script").innerHTML = "This text has been written inside the JavaScript."; // this changes budget everytime
+
+    if (transportation.value.target === 1){
+document.getElementById("transportation").innerHTML = (budget - 1500);
+        console.log(budget)
+    }
+    else if (transportation.value.target === 2){
+        document.getElementById("transportation").innerHTML = (budget - 2000);
+                console.log(budget)
+            }
+}
+
+function budget2(){
+    
+}
