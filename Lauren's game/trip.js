7833d5e665ce3b3cc4a6b2e6f1faa3fd2312d9cd
Seth <sethmoffat1@gmail.com>
2022-12-08 19:41:26 -0700

    patched bug

--- Lauren's game/trip.js
+++ Lauren's game/trip.js
@@ -1,7 +1,8 @@
 let budget = 10000
+let happiness = 100
 
 function budget1(){
-    document.getElementById("script").innerHTML = "This text has been written inside the JavaScript."; // this changes budget everytime
+    document.getElementById("transportation").innerHTML = "This text has been written inside the JavaScript."; // this changes budget everytime
 
     if (transportation.value.target === 1){
 document.getElementById("transportation").innerHTML = (budget - 1500);
